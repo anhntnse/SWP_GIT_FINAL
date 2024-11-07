846a26e8868e228a203541d817c6007e4c8ca43b
anhntnse <helloanhne@gmail.com>
2024-11-07 20:46:53 +0700

    up2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,20 +13,21 @@ import { setUserDetails } from './store/userSlice';
 function App() {
   const dispatch = useDispatch()
   const [cartProductCount,setCartProductCount] = useState(0)
+  const [userId, setUserId] = useState('')
 
   const fetchUserDetails = useCallback(async () => {
-    const dataResponse = await fetch(SummaryApi.current_user.url, {
-      method: SummaryApi.current_user.method,
-      credentials: 'include',
-    });
-  
-    const dataApi = await dataResponse.json();
-  
-    if (dataApi.success) {
-      dispatch(setUserDetails(dataApi.data));
-      return dataApi.data;
-    }
-  }, [dispatch]);
+  const dataResponse = await fetch(SummaryApi.current_user.url, {
+    method: SummaryApi.current_user.method,
+    credentials: 'include',
+  });
+
+  const dataApi = await dataResponse.json();
+
+  if (dataApi.success) {
+    dispatch(setUserDetails(dataApi.data));
+    return dataApi.data;
+  }
+}, [dispatch]);
 
   const fetchUserAddToCart = async()=>{
     const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
